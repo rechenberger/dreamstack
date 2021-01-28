8b7dbea609695476743aa169160c702d9572125f
rechenberger <rechenberger@me.com>
2021-01-28 16:35:59 +0000

    nx g @nx-extend/strapi:strapi

---
+++ apps/strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2d1f631ceca4a9881f592dc4915cf2d3'),
+    },
+  },
+});
