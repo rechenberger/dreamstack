d987c1811031003f173ca72f44bdddc047139a76
rechenberger <rechenberger@me.com>
2021-01-28 21:19:31 +0000

    strapi postgres

--- apps/strapi/config/database.js
+++ apps/strapi/config/database.js
@@ -1,15 +1,34 @@
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
+    // default: {
+    //   connector: 'bookshelf',
+    //   settings: {
+    //     client: 'sqlite',
+    //     filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+    //   },
+    //   options: {
+    //     useNullAsDefault: true,
+    //   },
+    // },
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        schema: env('DATABASE_SCHEMA', 'public'), // Not Required
+        // ssl: {
+        //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+        // },
+        ssl: false,
       },
       options: {
-        useNullAsDefault: true,
+        // ssl: env.bool('DATABASE_SSL', false),
       },
     },
   },
-});
+})
