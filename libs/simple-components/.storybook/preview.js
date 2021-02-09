7c167c16ddeb6d13a966b16f7595f3b52da60dcb
Tristan Rechenberger <rechenberger@me.com>
2021-02-09 17:41:44 +0100

    storybook tailwind

--- libs/simple-components/.storybook/preview.js
+++ libs/simple-components/.storybook/preview.js
@@ -1,4 +1,7 @@
+import React from 'react'
 import { addDecorator } from '@storybook/react'
 import { withKnobs } from '@storybook/addon-knobs'
+import { Layout } from './layout'
 
 addDecorator(withKnobs)
+addDecorator((storyFn) => <Layout>{storyFn()}</Layout>)
