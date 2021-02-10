a8677bc1cec8814e7d4bfcd5a814ef5aff29ed4c
Tristan Rechenberger <rechenberger@me.com>
2021-02-10 15:21:56 +0100

    @dreamstack/theme init

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
     '<rootDir>/apps/next',
     '<rootDir>/libs/simple-components',
     '<rootDir>/libs/graphql',
+    '<rootDir>/libs/theme',
   ],
 }
---
+++ libs/theme/src/lib/theme.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+/* eslint-disable-next-line */
+export interface ThemeProps {}
+
+export function Theme(props: ThemeProps) {
+  return (
+    <div>
+      <h1>Welcome to theme!</h1>
+    </div>
+  )
+}
+
+export default Theme
