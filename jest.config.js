721f8296acd30c7a7385248c2b4741818ae4913c
Tristan Rechenberger <rechenberger@me.com>
2021-02-10 11:28:05 +0100

    @dreamstack/graphql init

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/next', '<rootDir>/libs/simple-components'],
+  projects: [
+    '<rootDir>/apps/next',
+    '<rootDir>/libs/simple-components',
+    '<rootDir>/libs/graphql',
+  ],
 }
---
+++ libs/graphql/src/lib/graphql.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+/* eslint-disable-next-line */
+export interface GraphqlProps {}
+
+export function Graphql(props: GraphqlProps) {
+  return (
+    <div>
+      <h1>Welcome to graphql!</h1>
+    </div>
+  )
+}
+
+export default Graphql
