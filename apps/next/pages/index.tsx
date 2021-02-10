bd06d714b223ac15f7c34554c9a5ce2823a5fbd0
Tristan Rechenberger <rechenberger@me.com>
2021-02-10 23:21:45 +0100

    IndexPage spacing

--- apps/next/pages/index.tsx
+++ apps/next/pages/index.tsx
@@ -1,17 +1,21 @@
 import { SimpleButton } from '@dreamstack/simple-components'
 import Link from 'next/link'
 import React from 'react'
-import styled from 'styled-components'
+import tw from 'tailwind-styled-components'
 
-const StyledPage = styled.div`
-  margin: 32px auto;
-  width: max-content;
+const StyledPage = tw.div`
+  mx-auto
+  my-8
+  w-max
+  flex
+  flex-col
+  gap-2
 `
 
 export function Index() {
   return (
     <StyledPage>
-      <h1 className="text-4xl mb-4">DreamStack</h1>
+      <h1 className="text-4xl">DreamStack</h1>
       <a
         href="https://github.com/rechenberger/dreamstack"
         target="_blank"
